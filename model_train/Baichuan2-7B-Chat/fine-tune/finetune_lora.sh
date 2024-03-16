5554d2933b4d02f6176b74a26adb1e927cb76853
Sakura501 <2359216537@qq.com>
2024-03-16 10:25:57 +0000

    baichuan2-change

---
+++ model_train/Baichuan2-7B-Chat/fine-tune/finetune_lora.sh
@@ -0,0 +1,29 @@
+hostfile=""
+include="localhost:0"
+data_path=""
+model_name="baichuan-inc/Baichuan2-7B-Chat"
+output_dir="../darkword-Baichuan2-7B-Chat"
+deepspeed --hostfile=$hostfile --include=$include fine-tune.py  \
+    --report_to "none" \
+    --data_path $data_path \
+    --model_name_or_path $model_name \
+    --output_dir $output_dir \
+    --model_max_length 512 \
+    --num_train_epochs 4 \
+    --per_device_train_batch_size 16 \
+    --gradient_accumulation_steps 1 \
+    --save_strategy epoch \
+    --learning_rate 2e-5 \
+    --lr_scheduler_type constant \
+    --adam_beta1 0.9 \
+    --adam_beta2 0.98 \
+    --adam_epsilon 1e-8 \
+    --max_grad_norm 1.0 \
+    --weight_decay 1e-4 \
+    --warmup_ratio 0.0 \
+    --logging_steps 1 \
+    --gradient_checkpointing True \
+    --deepspeed ds_config.json \
+    --bf16 True \
+    --tf32 True \
+    --use_lora True
