b82512afb7a44026f34014739c5d3157fde325a7
Sakura501 <2359216537@qq.com>
2024-03-17 10:25:19 +0000

    test.csv

--- model_train/ChatGLM3-6B/model_use.sh
+++ model_train/ChatGLM3-6B/model_use.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 # 似乎依赖不行，换个conda环境跑一下
 # conda activate chatglm3-use
-echo "y" | CUDA_VISIBLE_DEVICES=1 python3 finetune_demo/inference_hf.py darkword-ChatGLM3-6B/checkpoint-3000/ --prompt $1
+# echo "y" | CUDA_VISIBLE_DEVICES=1 python3 finetune_demo/inference_hf.py darkword-ChatGLM3-6B/checkpoint-3000/ --prompt $1
+CUDA_VISIBLE_DEVICES=1 python3 base_model_use.py
