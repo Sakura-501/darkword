05731b55a7d84554f5317eb068467176126209f7
Sakura501 <2359216537@qq.com>
2024-03-21 12:59:57 +0000

    judge_winner_finish

--- model_eval/winrate/run_winrate.py
+++ model_eval/winrate/run_winrate.py
@@ -33,18 +33,123 @@ def get_cosine_similarity(original_text1,original_text2):
 
     # 余弦相似度计算
     cos = cal_cosine_similarity(tf_idf1,tf_idf2)
-    print(cos)
+    # print(cos)
     return cos
 
-def lora_atom_eval(base_file,lora_file):
+def judge_winner(text,cos1,cos2):
+    winner_a="model_a"
+    winner_b="model_b"
+    tie="tie"
+    if cos1 > 0.6 or cos2 > 0.6:
+        if cos1 > cos2:
+            return winner_a
+        elif cos2 > cos1:
+            return winner_b
+        else:
+            return tie
+    # 如果tf-idf余弦相似度都没有超过0.6，就需要研究者自行判断
+    else:
+        print("question: \n"+text[0])
+        print("standard_answer: \n"+text[1])
+        print(f"model_a_answer: {cos1}\n"+text[2])
+        print(f"model_b_answer: {cos2}\n"+text[3])
+        choice = input("请选择 1.model_a胜出 2.model_b胜出 3.tie平手\n")
+        if choice == "1":
+            return winner_a
+        elif choice == "2":
+            return winner_b
+        return tie
+
+def base_and_lora_eval(base_file,lora_file,choice,output_file):
     with open(base_file,"r",encoding="utf-8") as basefile:
-        base_data = json.load(basefile)
-        print(base_data)
+        base_responses = json.load(basefile)
+    basefile.close()
+    with open(lora_file,"r",encoding="utf-8") as lorafile:
+        lora_responses = json.load(lorafile)
+    lorafile.close()
+    with open(output_file,"wt",encoding="utf-8") as output:
+        for one_base_response,one_lora_response in zip(base_responses,lora_responses):
+            # base_vs_atom
+            if choice == 1:
+                one_result={"model_a":"atom_base","model_b":"atom_lora"}
+                text0 = one_base_response["question"]
+                text1 = one_base_response["standard_answer"]
+                text2 = one_base_response["atom_base_response"]
+                text3 = one_lora_response["atom_lora_response"]
+                text=[text0,text1,text2,text3]
+                cos1 = get_cosine_similarity(text1,text2)
+                cos2 = get_cosine_similarity(text1,text3)
+                # print(cos1,cos2)
+                winner = judge_winner(text,cos1,cos2)
+                one_result["winner"] = winner
+                output.write(json.dumps(one_result,ensure_ascii=False)+"\n")
+            # base_vs_baichuan2
+            elif choice == 2:
+                one_result={"model_a":"atom_base","model_b":"baichuan2_lora"}
+                text0 = one_base_response["question"]
+                text1 = one_base_response["standard_answer"]
+                text2 = one_base_response["atom_base_response"]
+                text3 = one_lora_response["baichuan2_lora_response"]
+                text=[text0,text1,text2,text3]
+                cos1 = get_cosine_similarity(text1,text2)
+                cos2 = get_cosine_similarity(text1,text3)
+                # print(cos1,cos2)
+                winner = judge_winner(text,cos1,cos2)
+                one_result["winner"] = winner
+                output.write(json.dumps(one_result,ensure_ascii=False)+"\n")
+            # base_vs_chatglm3
+            elif choice == 3:
+                one_result={"model_a":"atom_base","model_b":"chatglm3_lora"}
+                text0 = one_base_response["question"]
+                text1 = one_base_response["standard_answer"]
+                text2 = one_base_response["atom_base_response"]
+                text3 = one_lora_response["chatglm3_lora_response"]
+                text=[text0,text1,text2,text3]
+                cos1 = get_cosine_similarity(text1,text2)
+                cos2 = get_cosine_similarity(text1,text3)
+                # print(cos1,cos2)
+                winner = judge_winner(text,cos1,cos2)
+                one_result["winner"] = winner
+                output.write(json.dumps(one_result,ensure_ascii=False)+"\n")
+            # baichuan2_vs_chatglm3
+            else:
+                one_result={"model_a":"baichuan2_lora","model_b":"chatglm3_lora"}
+                text0 = one_base_response["question"]
+                text1 = one_base_response["standard_answer"]
+                text2 = one_base_response["baichuan2_lora_response"]
+                text3 = one_lora_response["chatglm3_lora_response"]
+                text=[text0,text1,text2,text3]
+                cos1 = get_cosine_similarity(text1,text2)
+                cos2 = get_cosine_similarity(text1,text3)
+                # print(cos1,cos2)
+                winner = judge_winner(text,cos1,cos2)
+                one_result["winner"] = winner
+                output.write(json.dumps(one_result,ensure_ascii=False)+"\n")
+    output.close()
+        
     
-if __name__ == "main":
-    base_atom_response_file=""
-    lora_atom_response_file=""
-    lora_baichuan2_response_file=""
-    lora_chatglm3_response_file=""
-    lora_atom_eval(base_atom_response_file,lora_atom_response_file)
+if __name__ == "__main__":
+    base_atom_response_file="/home/w1nd/darkword/1darkword/model_eval/data/results/lora_baichuan2_response.json"
+    lora_atom_response_file="/home/w1nd/darkword/1darkword/model_eval/data/results/lora_chatglm3_response.json"
+    lora_baichuan2_response_file="/home/w1nd/darkword/1darkword/model_eval/data/results/lora_baichuan2_response.json"
+    lora_chatglm3_response_file="/home/w1nd/darkword/1darkword/model_eval/data/results/lora_chatglm3_response.json"
+    
+    base_atom_vs_lora_atom_file="/home/w1nd/darkword/1darkword/model_eval/data/winner/base_atom_vs_lora_atom.json"
+    base_atom_vs_lora_baichuan2_file="/home/w1nd/darkword/1darkword/model_eval/data/winner/base_atom_vs_lora_baichuan2.json"
+    base_atom_cs_lora_chatglm3_file="/home/w1nd/darkword/1darkword/model_eval/data/winner/base_atom_cs_lora_chatglm3.json"
+    test_file="/home/w1nd/darkword/1darkword/model_eval/data/winner/test.json"
+    
+    
+    # 1.base_atom_vs_lora_atom
+    # base_and_lora_eval(base_atom_response_file,lora_atom_response_file,1,base_atom_vs_lora_atom_file)
+    
+    # 2.base_atom_vs_lora_baichuan2
+    # base_and_lora_eval(base_atom_response_file,lora_baichuan2_response_file,2,base_atom_vs_lora_baichuan2_file)
+    
+    # 3.base_atom_vs_lora_chatglm3
+    # base_and_lora_eval(base_atom_response_file,lora_chatglm3_response_file,3,base_atom_cs_lora_chatglm3_file)
+    
+    # 4.lora_baichuan2_vs_lora_chatglm3
+    # base_and_lora_eval(lora_baichuan2_response_file,lora_chatglm3_response_file,4,test_file)
+    
     
